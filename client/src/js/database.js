edc5f2759e0faf1f9be74e9ab4ec0ae9b050940b
aldreanelafont <93751029+aldreanelafont@users.noreply.github.com>
2022-06-22 23:42:53 -0700

    initial commit

---
+++ client/src/js/database.js
@@ -0,0 +1,38 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  console.log('Putting data into the database');
+  const jateData = await openDB('jate', 1);
+  const tx = jateData.transaction('jate', 'readwrite');
+  const objectStr = tx.objectStore('jate');
+  const req = objectStr.put({ id: 1, value: content });
+  const result = await req;
+  console.log('Data successfully saved to the database!', result);
+};
+
+
+// TODO: Add logic for a method that gets all the content from the database - GET, DONE 
+export const getDb = async () => {
+console.log('GETting data from the database');
+const jateData = await openDB('jate', 1);
+const tx = jateData.transaction('jate', 'readonly');
+const objectStr = tx.objectStore('jate');
+const req = objectStr.get(1);
+const result = await req; 
+console.log('Data successfully saved to the database', result); 
+};
+
+initdb();
